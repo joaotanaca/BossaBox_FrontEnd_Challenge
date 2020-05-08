9f06821d6bdeddbea21d7f75796c1af8bd970edb
Tanaca <jtanacar@gmail.com>
2020-05-08 00:28:07 -0300

    Titulo e subtitulo adicionados, começando pesquisa

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, ContainerTools, ToolsTitle, ToolsSubtitle } from './styles';
+import Search from '../../Componentes/SearchBar';
 
 const Index = () => (
   <Container>
+    <ContainerTools>
+      <ToolsTitle>VUTTR</ToolsTitle>
+      <ToolsSubtitle>Very Useful Tools to Remember</ToolsSubtitle>
+      <Search />
+    </ContainerTools>
   </Container>
 );
 
--- src/pages/Index/styles.js
+++ src/pages/Index/styles.js
@@ -1,8 +1,28 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  width:100%;
-  display:flex;
-  justify-content:center;
-  align-items:center;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Source Sans Pro', sans-serif;
+  color: #170c3a;
+`;
+
+export const ContainerTools = styled.div`
+  max-width: 800px;
+  width: 80%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex-flow: column wrap;
+`;
+
+export const ToolsTitle = styled.h1`
+  font-size: 42px;
+`;
+
+export const ToolsSubtitle = styled.h3`
+  font-size: 30px;
+  margin-top: 0;
 `;

7efa3f104d781058db069ef24c39a597f063b737
Tanaca <jtanacar@gmail.com>
2020-05-09 13:58:22 -0300

    Barra de pesquisa com checkbox e remoção da tool

--- src/Componentes/SearchBar/index.js
+++ src/Componentes/SearchBar/index.js
@@ -1,16 +1,118 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../api'
+import Modal from '../Modal';
 
-import { ContainerSearchBar, InputSearchBar, IconSearch } from './styles';
+import {
+  ContainerSearchBar,
+  InputSearchBar,
+  IconInputSearch,
+  ContainerCheckboxSearch,
+  ButtonAddSearchBar,
+  IconButtonAdd,
+  ContainerTools,
+  ContainerTool,
+  TitleTool,
+  DescriptionTool,
+  TagsToolContainer,
+  TagTool,
+  RemoveTool,
+  RemoveIconTool,
+  ModalTitle,
+  ModalDescription,
+  ModalConfirmRemove,
+  CloseIconModal,
+  ModalConfirmRemoveContainer,
+} from './styles';
 import CheckboxSearchBar from '../CheckboxSearchBar';
 
 function SearchBar() {
+  const [tools, setTools] = useState([]);
+  const [query, setQuery] = useState('');
+  const [tagsOnly, setTagsOnly] = useState(false);
+  const [showRemove, setShowRemove] = useState(false);
+  const [showAdd, setShowAdd] = useState(false);
+  const [remove, setRemove] = useState(true);
+  const [removeId, setRemoveId] = useState('');
+  const [removeName, setRemoveName] = useState('');
+
+  function showVerification(show) {
+    if(show==='remove'){
+      setShowRemove(!showRemove);
+
+    }else{
+      setShowAdd(!showAdd)
+    }
+  }
+
+  async function removeTool() {
+    const response = await api.delete(
+      `tools/${removeId}`
+    );
+    setRemove(response)
+    showVerification('remove');
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      const { data } = await api.get(
+        tagsOnly
+          ? `tools?tags_like=${query}`
+          : query
+          ? `tools?q=${query}`
+          : 'tools'
+      );
+      setTools(data);
+    }
+    fetchData();
+  }, [query, tagsOnly, remove]);
+
   return (
     <>
       <ContainerSearchBar>
-        <IconSearch />
-        <InputSearchBar />
-        <CheckboxSearchBar label={"search in tags only"} />
+        <IconInputSearch />
+        <InputSearchBar onChange={e => setQuery(e.target.value)} />
+        <ContainerCheckboxSearch onClick={() => setTagsOnly(!tagsOnly)}>
+          <CheckboxSearchBar
+            label={'search in tags only'}
+            tagsOnly={tagsOnly}
+          />
+        </ContainerCheckboxSearch>
+        <ButtonAddSearchBar>
+          <IconButtonAdd /> Add
+        </ButtonAddSearchBar>
       </ContainerSearchBar>
+      <ContainerTools>
+        {tools.map(tool => (
+          <ContainerTool key={tool.id}>
+            <TitleTool href={tool.link}>{tool.title}</TitleTool>
+            <RemoveTool
+              onClick={() => {
+                showVerification('remove');
+                setRemoveName(tool.title);
+                setRemoveId(tool.id);
+              }}
+            >
+              <RemoveIconTool /> remove
+            </RemoveTool>
+            <DescriptionTool>{tool.description}</DescriptionTool>
+            <TagsToolContainer>
+              {tool.tags.map((tag, index) => (
+                <TagTool key={index}>#{tag}</TagTool>
+              ))}
+            </TagsToolContainer>
+          </ContainerTool>
+        ))}
+      </ContainerTools>
+      <Modal show={showRemove}>
+        <CloseIconModal onClick={()=>{showVerification('remove')}} />
+        <ModalTitle>Remove tool</ModalTitle>
+        <ModalDescription>
+          Are you sure you want to remove {removeName}?
+        </ModalDescription>
+        <ModalConfirmRemoveContainer>
+          <ModalConfirmRemove onClick={removeTool}>Confirm</ModalConfirmRemove>
+        </ModalConfirmRemoveContainer>
+      </Modal>
     </>
   );
 }
--- src/Componentes/SearchBar/styles.js
+++ src/Componentes/SearchBar/styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { FiSearch } from 'react-icons/fi';
+import { FiSearch, FiPlus, FiX } from 'react-icons/fi';
 
 export const ContainerSearchBar = styled.div`
   width: 100%;
@@ -9,7 +9,7 @@ export const ContainerSearchBar = styled.div`
   position: relative;
 `;
 
-export const IconSearch = styled(FiSearch)`
+export const IconInputSearch = styled(FiSearch)`
   position: absolute;
   top: 50%;
   left: 10px;
@@ -31,4 +31,120 @@ export const InputSearchBar = styled.input.attrs({
   ::placeholder {
     color: #b1adb9;
   }
+`;
+
+export const ContainerCheckboxSearch = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ButtonAddSearchBar = styled.button`
+  padding: 10px 10px;
+  padding-left: 25px;
+  background: #365df0;
+  border-radius: 5px;
+  color: white;
+  border: 0;
+  position: absolute;
+  right: 0px;
+  top: 50%;
+  transform: translate(0, -50%);
+  &:hover {
+    background: #2f55cc;
+  }
+  &:active {
+    background: #244aa8;
+  }
+`;
+
+export const IconButtonAdd = styled(FiPlus)`
+  position: absolute;
+  top: 50%;
+  left: 7px;
+  transform: translate(0, -50%);
+`;
+
+export const ContainerTools = styled.div`
+  width: 100%;
+`;
+
+export const ContainerTool = styled.div`
+  width: calc(100% - 40px);
+  margin: 20px 0;
+  background: #ffffff 0% 0% no-repeat padding-box;
+  box-shadow: 0px 5px 7px #0000000d;
+  border: 1px solid #ebeaed;
+  border-radius: 5px;
+  padding: 20px;
+  position: relative;
+`;
+
+export const TitleTool = styled.a.attrs({
+  target: '_blank',
+})`
+  color: #365df0;
+`;
+
+export const DescriptionTool = styled.p``;
+
+export const TagsToolContainer = styled.div``;
+
+export const TagTool = styled.b`
+  margin: 0 5px;
+  &:first-child {
+    margin-left: 0;
+  }
+`;
+
+export const RemoveTool = styled.b`
+  position: absolute;
+  right: 15px;
+  top: 15px;
+  cursor: pointer;
+`;
+
+export const RemoveIconTool = styled(FiX)`
+  position: absolute;
+  top: 50%;
+  left: -17px;
+  transform: translate(0, -50%);
+`;
+
+export const ModalTitle = styled.h4`
+  font-size: 26px;
+  margin-top: 0;
+`;
+
+export const ModalDescription = styled.p`
+  font-size: 20px;
+  color: #8f8a9b;
+`;
+
+export const ModalConfirmRemoveContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const ModalConfirmRemove = styled.button`
+  background: #f95e5a;
+  color: white;
+  border-radius: 5px;
+  border: 0;
+  padding: 10px 20px;
+  &:hover {
+    background: #cc4c4c;
+  }
+  &:active {
+    background: #a53f3f;
+  }
+`;
+
+export const CloseIconModal = styled(FiX)`
+  position: absolute;
+  right: 20px;
+  top: 25px;
+  font-size: 20px;
+  cursor: pointer;
 `;

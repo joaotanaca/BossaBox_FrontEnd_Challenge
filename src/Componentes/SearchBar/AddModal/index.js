6917472d0ae639531856e8ce05ea56ed0109d9e0
Joao Tanaca <tanaca@pop-os.localdomain>
2020-05-11 15:09:45 -0300

    Modal adicionar Tool criada

--- src/Componentes/SearchBar/AddModal/index.js
+++ src/Componentes/SearchBar/AddModal/index.js
@@ -1,28 +1,110 @@
-import React from 'react';
+import React, { useState, createRef } from 'react';
 
 import {
   ContainerAddModal,
+  TitleAddModal,
+  IconAddModal,
   AddInputTool,
   AddInputToolDescription,
   AddInputToolLink,
   LabelAddInputs,
+  ContainerAddButtonPost,
+  AddButtonPost,
 } from './styles';
+import InputTags from './InputTags';
 
-function AddModal() {
+function AddModal({ addTool }) {
+  const [name, setName] = useState('');
+  const [link, setLink] = useState('');
+  const [description, setDescription] = useState('');
+  const [err, setErr] = useState([]);
+  const [tags, setTags] = useState([]);
+  const refTagsInput = createRef();
+
+  function handleKeyPress({ key, target: { value } }) {
+    if (key === 'Enter') {
+      setTags([...tags, value.toLowerCase()]);
+      refTagsInput.current.value = '';
+    }
+  }
+
+  function deleteArrayTags(tag, index) {
+    const tagsArr = [...tags];
+    const indexArr = tagsArr.indexOf(tag);
+    tagsArr.splice(indexArr, 1);
+    setTags(tagsArr);
+  }
+
+  async function postToolData() {
+    if (!name || !link || !description) {
+      let errArray = {};
+      if (!name) {
+        errArray = { ...errArray, name: true };
+      }
+      if (!link) {
+        errArray = { ...errArray, link: true };
+      }
+      if (!description) {
+        errArray = { ...errArray, description: true };
+      }
+      if (tags.length === 0) {
+        errArray = { ...errArray, tags: true };
+      }
+      return setErr(errArray);
+    }
+    const response = addTool({
+      title: name,
+      link: link,
+      description: description,
+      tags: tags,
+    });
+    console.log(response);
+  }
   return (
     <ContainerAddModal>
+      <TitleAddModal>
+        <IconAddModal /> Add new tool
+      </TitleAddModal>
       <LabelAddInputs>
         Tool Name
-        <AddInputTool placeholder="Tool" />
+        <AddInputTool
+          placeholder="Name"
+          onChange={e => {
+            setName(e.target.value);
+          }}
+          className={err.name ? 'error' : ''}
+        />
       </LabelAddInputs>
       <LabelAddInputs>
         Tool Link
-        <AddInputToolLink placeholder="Tool link" />
+        <AddInputToolLink
+          placeholder="Link"
+          onChange={e => {
+            setLink(e.target.value);
+          }}
+          className={err.link ? 'error' : ''}
+        />
       </LabelAddInputs>
       <LabelAddInputs>
         Tool Description
-        <AddInputToolDescription placeholder="Tool Description" />
+        <AddInputToolDescription
+          placeholder="Description"
+          onChange={e => {
+            setDescription(e.target.value);
+          }}
+          className={err.description ? 'error' : ''}
+        />
       </LabelAddInputs>
+      <InputTags
+        tags={tags}
+        refTagsInput={refTagsInput}
+        deleteArrayTags={deleteArrayTags}
+        handleKeyPress={handleKeyPress}
+        errTags={err.tags}
+      />
+      <ContainerAddButtonPost>
+        <AddButtonPost onClick={postToolData}>Add tool</AddButtonPost>
+      </ContainerAddButtonPost>
     </ContainerAddModal>
   );
 }
--- src/Componentes/SearchBar/AddModal/styles.js
+++ src/Componentes/SearchBar/AddModal/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { FiPlus } from 'react-icons/fi';
 
 export const ContainerAddModal = styled.div`
   display: flex;
@@ -11,7 +12,27 @@ export const ContainerAddModal = styled.div`
       margin: 10px 0;
     }
   }
+  .error {
+    border: solid 1px #f95e5a;
+    color: #f95e5a;
+    background: #feefee;
+    &::placeholder {
+      color: #f95e5a;
+    }
+  }
 `;
+export const TitleAddModal = styled.h3`
+  width: 95%;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+`;
+
+export const IconAddModal = styled(FiPlus)`
+  margin-right: 5px;
+  font-weight: bold;
+`;
+
 export const AddInputTool = styled.input.attrs({
   type: 'text',
 })`
@@ -20,6 +41,7 @@ export const AddInputTool = styled.input.attrs({
   height: 30px;
   border-radius: 5px;
   padding-left: 5px;
+  outline: none;
   ::placeholder {
     color: #b1adb9;
   }
@@ -43,8 +65,42 @@ export const AddInputToolDescription = styled.textarea`
   padding-left: 5px;
   height: 100px;
   resize: none;
+  font-family: 'Source Sans Pro';
   ::placeholder {
     color: #b1adb9;
+  }
+`;
+
+export const InputAddTag = styled.input`
+  border: solid #ebeaed 1px;
+  background: #f5f4f6;
+  padding-left: 5px;
+  height: 30px;
+  flex: 1;
+  border: 0;
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const ContainerAddButtonPost = styled.div`
+  width: 80%;
+  display: flex;
+  justify-content: flex-end;
+  margin: 10px 0;
+`;
+
+export const AddButtonPost = styled.button`
+  padding: 10px 10px;
+  background: #365df0;
+  border-radius: 5px;
+  color: white;
+  border: 0;
+  &:hover {
+    background: #2f55cc;
+  }
+  &:active {
+    background: #244aa8;
   }
 `;
 
